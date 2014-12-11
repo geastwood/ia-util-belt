d07002285add54f326e761e7b641eb6a1b8cd78f
Fei Liu <liu.feiwood@gmail.com>
2014-12-11 15:19:59 +0100

    add password

--- src/core/control.js
+++ src/core/control.js
@@ -11,28 +11,46 @@ api = module.exports = function() {
             var prompt;
             var userConfigFile = homeFolder + '/.ia/user.config.json',
                 userConfigs,
-                hasUsername = false;
+                hasUsername = false,
+                hasPassword = false,
+                promptConfigs = [];
 
             if (fs.existsSync(userConfigFile)) {
                 userConfigs = require(userConfigFile);
                 hasUsername = !!userConfigs.username;
-                fn(userConfigs.username);
+                hasPassword = !!userConfigs.password;
             }
 
             if (!hasUsername) {
-                prompt = require('prompt');
-                prompt.get([{
+                promptConfigs.push({
                     name: 'username',
                     required: true,
                     description: 'Please specify username',
                     pattern: /\w{1,20}/
-                }],
-                function(err, inputs) {
-                    var data = JSON.stringify({'username': inputs.username}, null, 4);
+                });
+            }
+            if (!hasPassword) {
+                promptConfigs.push({
+                    name: 'password',
+                    required: true,
+                    password: true,
+                    description: 'Please specify svn password',
+                    pattern: /\w{1,20}/
+                });
+            }
 
-                    if (!fs.existsSync(path.dirname(userConfigFile))) {
-                        fs.mkdirSync(path.dirname(userConfigFile));
-                    }
+            if (hasUsername && hasPassword) {
+                fn(userConfigs.username);
+            }
+
+            if (promptConfigs.length > 0) {
+                prompt = require('prompt');
+                prompt.get(promptConfigs,
+                function(err, inputs) {
+                    var data = JSON.stringify({
+                        'username': inputs.username || userConfigs.username,
+                        'password': inputs.password || ''
+                    }, null, 4);
 
                     fs.writeFile(userConfigFile, data, 'utf8', function(err) {
                         if (err) {
@@ -41,7 +59,7 @@ api = module.exports = function() {
                         console.log(chalk.green('SUCCESS\u0009(Set)\u0009') + '%s is saved to "%s"',
                                     inputs.username,
                                     userConfigFile);
-                        fn(inputs.username);
+                        fn(inputs.username||userConfigs.username);
                     });
                 });
             }
