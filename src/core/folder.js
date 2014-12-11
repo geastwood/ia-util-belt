371a8e7a62d104633c5c9f4be925a0054658efc2
Fei Liu <liu.feiwood@gmail.com>
2014-12-11 17:43:31 +0100

    add more folders

--- src/core/folder.js
+++ src/core/folder.js
@@ -15,6 +15,16 @@ var fs = require('fs'),
         IA({app: 'service', branch: 'trunk'}).path.getBasePath(),
         IA({app: 'service', branch: 'current'}).path.getBasePath(),
         IA({app: 'service', branch: 'release'}).path.getBasePath(),
+        path.join('data', 'Frontend_Public'),
+        path.join('data', 'Frontend_Public'),
+        path.join('data', 'Frontend_Public', 'download'),
+        path.join('data', 'Frontend_Public', 'download', 'attachments'),
+        path.join('data', 'Frontend_Public', 'download', 'reports'),
+        path.join('data', 'Frontend_Public', 'mailqueue'),
+        path.join('data', 'Frontend_Public', 'uq'),
+        path.join('data', 'Frontend_Public', 'uq', 'img'),
+        path.join('data', 'Frontend_Public', 'uq', 'report'),
+        path.join('data', 'Frontend_Public', 'uq', 'tmp')
     ];
 
 api = module.exports = function() {
